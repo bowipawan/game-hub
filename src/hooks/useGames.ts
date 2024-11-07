557adcabef5206a97d06363bcede4f28d202dee6
Wipawan T <wipawan.trk@gmail.com>
2024-11-07 12:49:25 +0700

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,7 +9,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
-  genres: Genre[];
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
